201dbb5e0f22ceb439783c56cfbe7812b2749984
Josh <jhint2424@gmail.com>
2018-04-05 11:06:38 -0700

    added spotify keys

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
+
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
 var inquirer = require('inquirer');
 var omdb = require('omdb');
-var Twitter = require('twitter');
+var Twitter = require('twitter');//codigyprophet
 var spotify = require('spotify');
+
+
+
