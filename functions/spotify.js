fb236ba8afaf6eedda73de4ed9acc65e28e37efe
Josh <jhint2424@gmail.com>
2018-04-07 15:25:23 -0700

    spotify closer

--- functions/spotify.js
+++ functions/spotify.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 var keys = require("../keys");
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
+function getSpotify(song)   {
+    spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data.tracks); 
+      });
 
-function getSpotify()   {
-    var spotify = new Spotify({
- 
-    
-});
 }
+
+module.exports = {getSpotify: getSpotify};
--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var twitterSomethingLikeThat = require("./functions/twitter")
+var spotifySomethingLikeThat = require("./functions/spotify")
 var keys = require("./keys");
 //var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
@@ -8,16 +9,24 @@ var keys = require("./keys");
 //codigyprophet
 //var spotify = require('spotify');
 var command = process.argv[2];
+var command2 = process.argv[3];
+var song = "";
+for (var i = 3; i < process.argv.length; i++)   {
+    //console.log(process.argv[i]);
+    song += process.argv[i] + " ";
 
+}
 
-//run code
+//run code RUNNNNNN he's coming!!!!!!
 if (command === "my-tweets") {
      twitterSomethingLikeThat.getTweets();
  }
 
 else if (command === "spotify-this-song")   {
-    
+    spotifySomethingLikeThat.getSpotify(song);
 } 
+
  else{
      console.log("Please input a correct command");
  }
+console.log(song);
