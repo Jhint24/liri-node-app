bed5a75f9ff85abd1eb6ad656c26cdba84216cc6
Josh <jhint2424@gmail.com>
2018-04-08 13:47:39 -0700

    adding movie function

--- liri.js
+++ liri.js
@@ -1,15 +1,19 @@
 require("dotenv").config();
-var twitterSomethingLikeThat = require("./functions/twitter")
-var spotifySomethingLikeThat = require("./functions/spotify")
-var whatSomethingLikeThat = require("./functions/whatitsays")
+var twitterSomethingLikeThat = require("./functions/twitter");
+var spotifySomethingLikeThat = require("./functions/spotify");
+var whatSomethingLikeThat = require("./functions/whatitsays");
+var movieSomethingLikeThat = require("./functions/movies");
 var keys = require("./keys");
 
 var command = process.argv[2];
 var command2 = process.argv[3];
 var song = "";//searchstring update
+var movie = "";
+
 for (var i = 3; i < process.argv.length; i++)   {
     //console.log(process.argv[i]);
     song += process.argv[i] + " ";
+    movie += process.argv[i] + " ";
 
 }
 
@@ -21,6 +25,10 @@ if (command === "my-tweets") {
 else if (command === "spotify-this-song")   {
     spotifySomethingLikeThat.getSpotify(song);
 } 
+
+else if (command === "movie-this")  {
+    movieSomethingLikeThat.getMovie(movie);
+}
 else if (command === "do-what-it-says") {
     whatSomethingLikeThat.whatItSays();
     
